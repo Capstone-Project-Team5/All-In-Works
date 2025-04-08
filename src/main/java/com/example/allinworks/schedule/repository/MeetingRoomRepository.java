dbd431f727b237d4553ee43121de4070bfb0c5c0
mjay <01087153603a@gmail.com>
2025-04-08 21:40:55 +0900

    create classes

---
+++ src/main/java/com/example/allinworks/schedule/repository/MeetingRoomRepository.java
@@ -0,0 +1,10 @@
+package com.example.allinworks.schedule.repository;
+
+import com.example.allinworks.schedule.domain.MeetingRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Integer> {
+
+}
---
+++ src/main/java/com/example/allinworks/schedule/service/ScheduleService.java
@@ -0,0 +1,7 @@
+package com.example.allinworks.schedule.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+}
